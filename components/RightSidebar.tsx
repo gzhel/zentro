292fe7203d3d094d0619a42b858d3eb1e0f4eee7
Gregory Zhelyabin <gregoryzhel@gmail.com>
2025-10-04 00:01:00 +0500

    #21 Finalizing UI

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -11,7 +11,7 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
   return (
     <aside className={"right-sidebar"}>
       <section className={"flex flex-col pb-8"}>
-        <div className={"profile-banner"} />
+        <div className={"h-[120px] w-full bg-blue-600"} />
         <div className={"profile"}>
           <div className={"profile-img"}>
             <span className={"text-5xl font-bold text-blue-500"}>
@@ -30,15 +30,6 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
       <section className={"banks"}>
         <div className={"flex w-full justify-between"}>
           <h2 className={"header-2"}>My Banks</h2>
-          <Link href={"/"} className={"flex gap-2"}>
-            <Image
-              src={"/icons/plus.svg"}
-              width={20}
-              height={20}
-              alt={"plus"}
-            />
-            <h2 className={"text-14 font-semibold text-gray-600"}>Add Bank</h2>
-          </Link>
         </div>
 
         {banks?.length > 0 && (
